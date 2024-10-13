ccf09959cd5d63fb08a7aca670689892f0246096
Tanvir <tanvirlaravel@gmail.com>
2024-10-13 17:30:34 +0600

    initail commit

---
+++ assets-ninja.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Plugin Name: Assets Ninja
+ * Plugin URI: https://tanvir-assets-ninja.com
+ * Description: Assets management in depth
+ * Version: 1.0.0
+ * Author: Md Tanvirul Islam
+ * Author URI: tnavir.github
+ * License: GPLv2 or later 
+ * Text Domain: assets-ninja
+ * Domain Path: /languages/
+ */
