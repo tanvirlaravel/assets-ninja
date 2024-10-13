e23f05c3ba2a7c43019106d19dacabaaf48a38c4
Tanvir <tanvirlaravel@gmail.com>
2024-10-13 17:54:23 +0600

    text domain added

--- assets-ninja.php
+++ assets-ninja.php
@@ -7,13 +7,17 @@
  * Author: Md Tanvirul Islam
  * Author URI: tnavir.github
  * License: GPLv2 or later 
- * Text Domain: assets-ninja
+ * Text Domain: assetsninja
  * Domain Path: /languages/
  */
 
  class AssetsNinja {
     function __construct(){
-        echo 'hi';
+        add_action( 'plugin_loaded', [$this, "assets"]);
+    }
+
+    public function assets(){
+        load_plugin_textdomain( "assetsninja", false, plugin_dir_url( __FILE__ ) . "/languages" );
     }
  }
 
